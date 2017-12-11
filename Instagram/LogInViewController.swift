14d532fc9ef093cf9b31f99899d580f7a7e2c7da
Harold Lomotey <haroldlomotey@gmail.com>
2017-12-11 00:50:36 -0500

    submission

--- Instagram/LogInViewController.swift
+++ Instagram/LogInViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
-
+import Parse
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func signInDidTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func signUpDidTapped(_ sender: Any) {
+    }
+    
+    
 }
 
---
+++ Instagram/Post.swift
@@ -0,0 +1,9 @@
+//
+//  Post.swift
+//  Instagram
+//
+//  Created by Harold  on 12/8/17.
+//  Copyright © 2017 Harold . All rights reserved.
+//
+
+import Foundation
